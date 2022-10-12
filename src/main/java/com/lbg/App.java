ee035bc8b9ab078e176fcf84ca26e043a1ec965e
Kuda Z <kudawillz@hotmail.com>
2022-10-12 22:42:59 +0100

    Update App.java

--- src/main/java/com/lbg/App.java
+++ src/main/java/com/lbg/App.java
@@ -9,7 +9,9 @@ public class App
     public static void main( String[] args )
     {
         VATCalculator vc = new VATCalculator();
+        Accumulator ac = new Accumulator();
         
         System.out.println( "VAT: " + vc.calculateVATToPay(100, 20 ));
+        System.out.println( "Sum: " + ac.sum(2.0, 3.5));
     }
 }
